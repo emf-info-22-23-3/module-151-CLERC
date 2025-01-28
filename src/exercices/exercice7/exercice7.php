a129042a75b84c1319acafd39dc77412fb1b5183
Donovan <donovan.clerc@studentfr.ch>
2025-01-28 11:26:07 +0100

    exo 07

--- src/exercices/exercice7/exercice7.php
+++ src/exercices/exercice7/exercice7.php
@@ -1,11 +1,10 @@
 <?php
-	$bdd = new PDO('mysql:host=localhost;dbname=nomDB', 'root', 'pwd');
-	$reponse = 0;
-	
-	
-	while (true)
-	{
-	
-	}
-	$reponse->closeCursor();
+$bdd = new PDO('mysql:host=localhost;dbname=nomDB', 'root', 'pwd');
+$reponse = $bdd->query('select pk_jeux_video, titre from jeux_video');
+
+while ($row = $response->fetch()) {
+	echo $row['titre'] . $row['pk_jeux_video'];
+}
+
+$reponse->closeCursor();
 ?>
